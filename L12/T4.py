de9427cdb6970be90c1ef8ddd609861cff0e9e7a
Mr M Chen <145968094+MChenGHS@users.noreply.github.com>
2024-12-09 01:56:11 +0000

    Update T4.py

---
+++ L12/T4.py
@@ -0,0 +1,8 @@
+"""
+It's 01:53 right now. Mr Chen hasn't done this bit yet because he spent the entire weekend playing computer games.
+
+Just kidding.
+I've of course done this.
+The reason why it's not here yet is because I want you to try work the task out yourself, rather than relying on the solutions with little thinking.
+The soultion will be posted here later.
+"""
