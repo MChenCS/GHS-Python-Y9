41583e8f13a1681371ef3be52ad8387eb87372d4
Mr M Chen <145968094+MChenGHS@users.noreply.github.com>
2024-12-09 11:17:38 +0000

    Create T2.py

---
+++ L12/T2.py
@@ -0,0 +1,32 @@
+def hangman_display(chosen_word, unguessed_word):
+    for char in chosen_word:
+        if char in unguessed_word:
+            chosen_word = chosen_word.replace(char, "_")
+    print(chosen_word)
+
+import random
+
+wordlist = ["apple", "cocoa", "coach", "print"]
+chosen_word = random.choice(wordlist)
+temp = chosen_word
+
+lives = 8
+
+while lives != 0:
+    print("You have", lives, "lives left.")
+    chosen_char = input("What character are you guessing? ")
+    if chosen_char in chosen_word:
+        print("Right")
+        temp = temp.replace(chosen_char, "")
+    else:
+        print("Wrong")
+    lives = lives - 1
+    hangman_display(chosen_word, temp)
+    
+    if temp == "":
+        break
+
+if temp == "":
+    print("You won!")
+else:
+    print("You lose")
