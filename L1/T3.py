68339622f8dcb4ca0f47e8b2907e07612cc7c597
Mr M Chen <145968094+MChenCS@users.noreply.github.com>
2025-06-25 21:06:15 +0100

    Added L1-T2/3.py

---
+++ L1/T3.py
@@ -0,0 +1,8 @@
+# This code will not work. The variable 'name' is not defined before it is used.
+# To fix this, we need to define 'name' before we use it.
+print("Hello", name)
+name = "Bavey"
+
+# See below for the corrected code:
+name = "Bavey"
+print("Hello", name)
