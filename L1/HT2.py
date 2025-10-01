0a30722159c766df4a94733c91ce9136c1bef6f4
Mr M Chen <145968094+MChenCS@users.noreply.github.com>
2025-10-01 13:28:56 +0000

    Rebased L1 code

---
+++ L1/HT2.py
@@ -0,0 +1,9 @@
+name = "Bavey"
+print("Hi, my name is ", name)
+age = 12
+print("I'm ", age, " years old")
+favourite_food = "pizza"
+print("My favourite food is ", favourite_food)
+
+favourite_colour = "blue"
+print("My favourite colour is ", favourite_colour)
---
+++ L1/LT2.py
@@ -0,0 +1,6 @@
+name = "Bavey"
+print("Hi, my name is ", name)
+age = 12
+print("I'm ", age, " years old")
+favourite_food = "pizza"
+print("My favourite food is ", favourite_food)
