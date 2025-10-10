883e1cf76242eb45ec71ff501a6dae40ce917552
Mr M Chen <145968094+MChenCS@users.noreply.github.com>
2025-10-10 09:09:13 +0000

    Added L11 helper script

---
+++ L11/infinite_loop.py
@@ -0,0 +1,5 @@
+mood = input("How are you feeling today? ")
+while mood.lower() != "happy":
+    print("Sorry to hear that you're not happy. Let me tell you a joke to cheer you up!")
+    print("Why don't scientists trust atoms? Because they make up everything!")
+print("Yay! I'm glad you're feeling happy now!")
