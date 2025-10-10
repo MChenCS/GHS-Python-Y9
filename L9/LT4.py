492c558c0281ee1d26603dc1018892ab45373774
Mr M Chen <145968094+MChenCS@users.noreply.github.com>
2025-10-10 11:14:01 +0000

    Started L9 tasks

---
+++ L9/LT4.py
@@ -0,0 +1,1 @@
+eu_member_states = ["Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"]
